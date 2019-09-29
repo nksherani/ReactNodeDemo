e6bad9cd5004438ba0402a8e99d56c5b9a17b7a2
naveed <nksherani@outlook.com>
2019-09-30 01:24:19 +0500

    Login as react component with redirect on success

--- front-end/src/Auth/Login.js
+++ front-end/src/Auth/Login.js
@@ -7,69 +7,97 @@ import 'mdbreact/dist/css/mdb.css';
 import '../fontawesome/css/all.css'
 const API_URL = 'http://localhost:5000';
 
-function mySubmitHandler(e){
-    e.preventDefault();
-    var email = document.querySelector("#email");
-    var password = document.querySelector("#password");
-    var bodyFormData = {
-        'email': email.value,
-        'password': password.value
+class Login extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            redirect: false
+          }
     }
-    const url_ = `${API_URL}/auth/login/`;
-    axios({
-        method: 'post',
-        url: url_,
-        data: bodyFormData,
-        config: { headers: {'Content-Type': 'application/json' }}
+    
+      setRedirect = () => {
+        this.setState({
+          redirect: true
         })
-        .then(function (response) {
-            //handle success
-            var auth_token = response.data["token"];
-            localStorage.setItem("AUTH_TOKEN",auth_token);
-            console.log(response);
-            this.props.history.push('/')
-            //window.location.href='localhost:3000';
-        })
-        .catch(function (response) {
-            //handle error
-            console.log(response);
-        });
+      }
+      renderRedirect = () => {
+        if (this.state.redirect) {
+          return <Redirect to='/' />
+        }
+      }
+    mySubmitHandler=(e)=>{
+        e.preventDefault();
+        var email = document.querySelector("#email");
+        var password = document.querySelector("#password");
+        var bodyFormData = {
+            'email': email.value,
+            'password': password.value
+        }
+        const url_ = `${API_URL}/auth/login/`;
+        var self=this;
+        axios({
+            method: 'post',
+            url: url_,
+            data: bodyFormData,
+            config: { headers: {'Content-Type': 'application/json' }}
+            })
+            .then(function (response) {
+                //handle success
+                var auth_token = response.data["token"];
+                localStorage.setItem("AUTH_TOKEN",auth_token);
+                console.log(response);
+                self.setRedirect();
+                //this.props.history.push('/')
+                //window.location.href='localhost:3000';
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+    }
+    
+    render = () => {
+        return (
+            <div>
+                {this.renderRedirect()}
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol md="6">
+                <form  onSubmit={this.mySubmitHandler}>
+                  <p className="h5 text-center mb-4">Sign in</p>
+                  <div className="grey-text">
+                    <MDBInput
+                      id="email"
+                      label="Type your email"
+                      icon="envelope"
+                      group
+                      type="email"
+                      validate
+                      error="wrong"
+                      success="right"
+                    />
+                    <MDBInput
+                      id="password"
+                      label="Type your password"
+                      icon="lock"
+                      group
+                      type="password"
+                      validate
+                    />
+                  </div>
+                  <div className="text-center">
+                    <MDBBtn type="submit">Login</MDBBtn>
+                  </div>
+                </form>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+          </div>
+        );
+      };
+      
 }
-const FormPage = () => {
-  return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form  onSubmit={mySubmitHandler}>
-            <p className="h5 text-center mb-4">Sign in</p>
-            <div className="grey-text">
-              <MDBInput
-                id="email"
-                label="Type your email"
-                icon="envelope"
-                group
-                type="email"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                id="password"
-                label="Type your password"
-                icon="lock"
-                group
-                type="password"
-                validate
-              />
-            </div>
-            <div className="text-center">
-              <MDBBtn type="submit">Login</MDBBtn>
-            </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
-};
+export default Login;
 
-export default FormPage;
